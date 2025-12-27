7d17520bf0cc115542ed7d4687ae8ad29f14f28d
Julio Cesar <hi@jcsx.dev>
2025-12-26 23:02:46 -0300

    chore: bump version to v0.1.0
    Synchronize version constants (version, commit hash, and date) to
    reflect the v0.1.0 release state.

--- src/letsgolang.sh
+++ src/letsgolang.sh
@@ -17,9 +17,9 @@ set -u
 
 # VERSION BLOCK (managed by scripts/revision.sh)
 # These constants are updated automatically during the release process.
-readonly G_SCRIPT_VERSION=
-readonly G_SCRIPT_COMMIT=
-readonly G_SCRIPT_DATE=
+readonly G_SCRIPT_VERSION='0.1.0'
+readonly G_SCRIPT_COMMIT='d7297bf'
+readonly G_SCRIPT_DATE='2025-12-27'
 # END VERSION BLOCK
 
 # SETTINGS BLOCK
